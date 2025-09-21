08afdc31894b64d4a0e7f1be3862ff952f36956c
Lab1 <fartem65456@gmail.com>
2025-09-21 15:55:10 +0500

    Методы лабораторной, класс ввода с проверкой, класс "панели пользователя"

---
+++ src/CheckInput.java
@@ -0,0 +1,191 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CheckInput {
+
+	Scanner in = new Scanner(System.in);
+
+	// INT
+
+	/**
+	 * Ввод целого числа с клавиатуры.
+	 *
+	 * @param inputMessage пользовательское сообщение для ввода числа
+	 */
+	public int inputInt(String inputMessage) {
+		boolean isInt = false;
+
+		int result = 0;
+
+		while (!isInt) {
+			System.out.print(inputMessage);
+			try {
+				result = in.nextInt();
+				isInt = true;
+			} catch (InputMismatchException error) {
+				System.out.println("Ошибка: введено не целое число.");
+				in.next();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Ввод целого числа с клавиатуры.
+	 *
+	 */
+	public int inputInt() {
+		return inputInt("Введите целое число: ");
+	}
+
+	/**
+	 * Ввод целого числа в диапазоне, не включительно
+	 *
+	 * @param inputMessage пользовательское сообщение для ввода числа
+	 */
+	public int inputIntRange(String inputMessage, int min, int max) {
+		boolean isInt = false;
+
+		int result = 0;
+
+		while (!isInt) {
+			System.out.print(inputMessage);
+			try {
+				result = in.nextInt();
+				if (result > +min && result < +max) {
+					isInt = true;
+				} else {
+					System.out.printf("Число %d не попадает в диапазон от %d до %d\n", result, min + 1,
+							max - 1);
+				}
+			} catch (InputMismatchException error) {
+				System.out.println("Ошибка: введено не целое число.");
+				in.next();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Ввод целого числа с ограничением слева, не включительно
+	 *
+	 * @param errorRangeMessage пользовательское сообщение о нарушении минимальной границы
+	 */
+	public int inputIntRangeMin(String inputMessage, String errorRangeMessage, int min) {
+		boolean isInt = false;
+
+		int result = 0;
+
+		while (!isInt) {
+			System.out.print(inputMessage);
+			try {
+				result = in.nextInt();
+				if (result > min) {
+					isInt = true;
+				} else {
+					System.out.printf(errorRangeMessage + "\n", min);
+				}
+			} catch (InputMismatchException error) {
+				System.out.println("Ошибка: введено не целое число.");
+				in.next();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Ввод целого числа с ограничением слева, не включительно
+	 *
+	 */
+	public int inputIntRangeMin(String inputMessage, int min) {
+		return inputIntRangeMin(inputMessage, "Вводимое число не должно быть меньше %d", min);
+	}
+
+	public int inputIntRangeMax(String inputMessage, int max) {
+		boolean isInt = false;
+
+		int result = 0;
+
+		while (!isInt) {
+			System.out.print(inputMessage);
+			try {
+				result = in.nextInt();
+				if (result < max) {
+					isInt = true;
+				} else {
+					System.out.printf("Вводимое число не должно быть Больше %d\n", max);
+				}
+			} catch (InputMismatchException error) {
+				System.out.println("Ошибка: введено не целое число.");
+				in.next();
+			}
+		}
+		return result;
+	}
+
+	//INT ARRAY
+
+	/**
+	 * Создание целочисленного массива
+	 */
+	public int[] createArrInt(String message) {
+		IO.println(message);
+		int len = inputIntRangeMin("Введите длину массива: ", "Длинна массива не может быть меньше %d!",
+				0);
+		int[] arr = new int[len];
+		for (int i = 0; i < len; i++) {
+			arr[i] = inputInt("Введите " + (i + 1) + "-ый элемент массива: ");
+		}
+		return arr;
+	}
+
+	public int[] createArrInt() {
+		return createArrInt("Создание массива: ");
+	}
+
+	//DOUBLE
+
+	/**
+	 * Ввод дробного числа
+	 *
+	 * @param inputMessage пользовательское сообщение для ввода числа
+	 */
+	public double inputDouble(String inputMessage) {
+		boolean isDouble = false;
+
+		double result = 0;
+
+		while (!isDouble) {
+			System.out.print(inputMessage);
+			try {
+				result = in.nextDouble();
+				isDouble = true;
+			} catch (InputMismatchException error) {
+				System.out.println("Ошибка: введено не число.");
+				in.next();
+			}
+		}
+		return result;
+	}
+
+	//CHAR
+
+	/**
+	 * Ввод символа-цифры
+	 */
+	public char inputCharDigit() {
+		boolean isDigit = false;
+
+		char result = 0;
+
+		while (!isDigit) {
+			System.out.print("Вводимый символ должен представлять собой один из “0 1 2 3 4 5 6 7 8 9”");
+
+			result = in.next().charAt(0);
+			if ((int) result >= 48 && (int) result <= 57) {
+				isDigit = true;
+			}
+		}
+		return result;
+	}
+}
---
+++ src/UserPanel.java
@@ -0,0 +1,263 @@
+public class UserPanel {
+
+	CheckInput input = new CheckInput();
+	Lab lab = new Lab();
+
+	private void showArray(int[] arr) {
+		IO.print("Результат: ");
+		for (int j : arr) {
+			IO.print(j + " ");
+		}
+	}
+
+	// Задача 1
+	private void t1C1() {
+		IO.println("Методы, задача 1");
+		double value = input.inputDouble("Введите дробное число: "), result = 0;
+		result = lab.fraction(value);
+		IO.println("Результат: " + result);
+	}
+
+	private void t1C2() {
+		IO.println("Условия, задача 1");
+		int value = input.inputInt(), result = 0;
+		result = lab.abs(value);
+		IO.println("Результат: " + result);
+	}
+
+	private void t1C3() {
+		IO.println("Циклы, задача 1");
+		int value = input.inputIntRangeMin("Введите целое число больше нуля: ", 0);
+		String result = "";
+		result = lab.listNums(value);
+		IO.println("Результат: " + result);
+	}
+
+	private void t1C4() {
+		IO.println("Массивы, задача 1");
+		int[] arr = input.createArrInt();
+		int value = input.inputInt(), result;
+		result = lab.findFirst(arr, value);
+		IO.println("Результат: " + result);
+	}
+
+	// задача 3
+	private void t2C1() {
+		IO.println("Методы, задача 3");
+		char value = input.inputCharDigit();
+		int result = 0;
+		result = lab.charToNum(value);
+		IO.println("Результат: " + result);
+	}
+
+	private void t2C2() {
+		IO.println("Условия, задача 3");
+		int value = input.inputInt();
+		boolean result = false;
+		result = lab.is35(value);
+		IO.println("Результат: " + result);
+	}
+
+	private void t2C3() {
+		IO.println("Циклы, задача 3");
+		int value = input.inputIntRangeMin("Введите целое число больше нуля: ", 0);
+		String result = "";
+		result = lab.chet(value);
+		IO.println("Результат: " + result);
+	}
+
+	private void t2C4() {
+		IO.println("Массивы, задача 3");
+		int[] arr = input.createArrInt();
+		int result = 0;
+		result = lab.maxAbs(arr);
+		IO.println("Результат: " + result);
+	}
+
+	// задача 5
+	private void t3C1() {
+		IO.println("Методы, задача 5");
+		int value = input.inputInt();
+		boolean result = false;
+		result = lab.is2Digits(value);
+		IO.println("Результат: " + result);
+	}
+
+	private void t3C2() {
+		IO.println("Условия, задача 5");
+		int x = input.inputInt(), y = input.inputInt(), z = input.inputInt(), result = 0;
+		result = lab.max3(x, y, z);
+		IO.println("Результат: " + result);
+	}
+
+	private void t3C3() {
+		IO.println("Циклы, задача 5");
+		long value = input.inputInt();
+		int result = 0;
+		result = lab.numLen(value);
+		IO.println("Результат: " + result);
+	}
+
+	private void t3C4() {
+		IO.println("Массивы, задача 5");
+		int[] result, arr = input.createArrInt("Создание массива 1"), ins = input.createArrInt(
+				"Создание массива 2");
+		int pos = input.inputIntRange("Введите позицию элемента от 0 до " + (arr.length) + ": ", -1,
+				arr.length + 1);
+		result = lab.add(arr, ins, pos);
+		IO.println("Позиция " + pos);
+		showArray(result);
+	}
+
+	// задача 7
+	private void t4C1() {
+		IO.println("Методы, задача 7");
+		int num = input.inputInt(), a = input.inputInt(), b = input.inputInt();
+		boolean result = false;
+		result = lab.isInRange(a, b, num);
+		IO.println("Результат: " + result);
+	}
+
+	private void t4C2() {
+		IO.println("Условия, задача 7");
+		int x = input.inputInt(), y = input.inputInt(), result = 0;
+		result = lab.sum2(x, y);
+		IO.println("Результат: " + result);
+	}
+
+	private void t4C3() {
+		IO.println("Циклы, задача 7");
+		int value = input.inputInt();
+		lab.square(value);
+	}
+
+	private void t4C4() {
+		IO.println("Массивы, задача 7");
+		int[] result, arr = input.createArrInt();
+		result = lab.reverseBack(arr);
+		showArray(result);
+	}
+
+	// задача 9
+	private void t5C1() {
+		IO.println("Методы, задача 9");
+		int a = input.inputInt(), b = input.inputInt(), c = input.inputInt();
+		boolean result = false;
+		result = lab.isEqual(a, b, c);
+		IO.println("Результат: " + result);
+	}
+
+	private void t5C2() {
+		IO.println("Условия, задача 9");
+		int value = input.inputInt();
+		String result = "";
+		result = lab.day(value);
+		IO.println("Результат: " + result);
+	}
+
+	private void t5C3() {
+		IO.println("Циклы, задача 9");
+		int value = input.inputInt();
+		lab.rightTriangle(value);
+
+	}
+
+	private void t5C4() {
+		IO.println("Массивы, задача 9");
+		int[] result, arr = input.createArrInt();
+		int value = input.inputInt();
+		result = lab.findAll(arr, value);
+		showArray(result);
+	}
+
+	public void choiceTask() {
+		int chapter, task;
+		IO.println("Выберите раздел: ");
+		IO.println("1 - Методы\n2 - Условия\n3 - Циклы\n4 - Массивы");
+		chapter = input.inputIntRange("Введите число от 1 до 4: ", 0, 5);
+		task = input.inputIntRange("Введите номер задачи от 1 до 5: ", 0, 6);
+
+		switch (task) {
+			case 1:
+				switch (chapter) {
+					case 1:
+						t1C1();
+						break;
+					case 2:
+						t1C2();
+						break;
+					case 3:
+						t1C3();
+						break;
+					case 4:
+						t1C4();
+						break;
+				}
+				break;
+			case 2:
+				switch (chapter) {
+					case 1:
+						t2C1();
+						break;
+					case 2:
+						t2C2();
+						break;
+					case 3:
+						t2C3();
+						break;
+					case 4:
+						t2C4();
+						break;
+				}
+				break;
+			case 3:
+				switch (chapter) {
+					case 1:
+						t3C1();
+						break;
+					case 2:
+						t3C2();
+						break;
+					case 3:
+						t3C3();
+						break;
+					case 4:
+						t3C4();
+						break;
+				}
+				break;
+			case 4:
+				switch (chapter) {
+					case 1:
+						t4C1();
+						break;
+					case 2:
+						t4C2();
+						break;
+					case 3:
+						t4C3();
+						break;
+					case 4:
+						t4C4();
+						break;
+				}
+				break;
+			case 5:
+				switch (chapter) {
+					case 1:
+						t5C1();
+						break;
+					case 2:
+						t5C2();
+						break;
+					case 3:
+						t5C3();
+						break;
+					case 4:
+						t5C4();
+						break;
+				}
+				break;
+		}
+	}
+}
